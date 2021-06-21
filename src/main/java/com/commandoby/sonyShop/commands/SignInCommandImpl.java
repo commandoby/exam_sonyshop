d3a08f6cd8c238f521d47004ea005f397ea6660a
commandoby <beresten20117@mail.ru>
2021-06-21 06:07:59 +0300

    HW_210618_4c

---
+++ src/main/java/com/commandoby/sonyShop/commands/SignInCommandImpl.java
@@ -0,0 +1,57 @@
+package com.commandoby.sonyShop.commands;
+
+import com.commandoby.sonyShop.classies.ShopContent;
+import com.commandoby.sonyShop.classies.User;
+import com.commandoby.sonyShop.exceptions.CommandException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static com.commandoby.sonyShop.enums.PagesPathEnum.SIGN_IN_PAGE;
+import static com.commandoby.sonyShop.enums.PagesPathEnum.REGISTER_PAGE;
+import static com.commandoby.sonyShop.enums.RequestParamEnum.*;
+
+public class SignInCommandImpl implements BaseCommand {
+    @Override
+    public String execute(HttpServletRequest servletRequest) throws CommandException {
+        HttpSession session = servletRequest.getSession();
+        session.setAttribute(EMAIL.getValue(), "");
+        session.setAttribute(PASSWORD.getValue(), "");
+
+        if (servletRequest.getParameter(EMAIL.getValue()) != null) {
+            if (!secondPasswordCheck(servletRequest)) {
+                servletRequest.setAttribute(INFO.getValue(), "Password mismatch");
+                return REGISTER_PAGE.getPath();
+            }
+
+            if (duplicateCheck(servletRequest)) {
+                servletRequest.setAttribute(INFO.getValue(), "User exists");
+                return REGISTER_PAGE.getPath();
+            }
+
+            String name = servletRequest.getParameter(NAME.getValue());
+            String surname = servletRequest.getParameter(SURNAME.getValue());
+            String data = servletRequest.getParameter(USER_DATA.getValue());
+            String email = servletRequest.getParameter(EMAIL.getValue());
+            String password = servletRequest.getParameter(PASSWORD.getValue());
+            ShopContent.setUser(new User(name, surname, email, password, data));
+        }
+
+        return SIGN_IN_PAGE.getPath();
+    }
+
+    private boolean secondPasswordCheck(HttpServletRequest servletRequest) {
+        String password = servletRequest.getParameter(PASSWORD.getValue());
+        String secondPassword = servletRequest.getParameter(SECOND_PASSWORD.getValue());
+        if (password.equals(secondPassword)) return true;
+        return false;
+    }
+
+    private boolean duplicateCheck(HttpServletRequest servletRequest) {
+        String email = servletRequest.getParameter(EMAIL.getValue());
+        for (User user : ShopContent.getUserList()) {
+            if (user.getEmail().equals(email)) return true;
+        }
+        return false;
+    }
+}
