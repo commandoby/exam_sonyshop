ce06c2e0a0d85882745f5cbce15939a0c429f8eb
commandoby <beresten20117@mail.ru>
2021-09-30 23:26:26 +0300

    v102.00011

--- src/test/java/com/commandoby/sonyShop/service/impl/OrderServiceImplTests.java
+++ src/test/java/com/commandoby/sonyShop/service/impl/OrderServiceImplTests.java
@@ -3,6 +3,7 @@ package com.commandoby.sonyShop.service.impl;
 import com.commandoby.sonyShop.components.Category;
 import com.commandoby.sonyShop.components.Order;
 import com.commandoby.sonyShop.components.Product;
+import com.commandoby.sonyShop.components.User;
 import com.commandoby.sonyShop.exceptions.NotFoundException;
 import com.commandoby.sonyShop.exceptions.ServiceException;
 import com.commandoby.sonyShop.repository.OrderRepository;
@@ -22,6 +23,7 @@ public class OrderServiceImplTests {
     private static OrderService orderService;
 
     private static Order order;
+    private static User user;
     private static Product product1;
     private static Product product2;
 
@@ -32,14 +34,18 @@ public class OrderServiceImplTests {
 
         orderService = new OrderServiceImpl(orderRepositoryMock, productServiceMock);
 
+        user = User.newBuilder().withName("User").withEmail("user").withBalance(10000).build();
+
         Category category = new Category("Phone", "phone", "phone.jpeg", 1);
 
         product1 = new Product("Sony Xperia 10 II XQ-AU52 Dual", "10_II_XQ-AU52_Dual.jpeg",
                 "Android, экран 6\" OLED (1080x2520), Qualcomm Snapdragon 665, ОЗУ 4 ГБ, флэш-память 128 ГБ, " +
-                        "карты памяти, камера 12 Мп, аккумулятор 3600 мАч, 2 SIM", category, 899, 24);
+                        "карты памяти, камера 12 Мп, аккумулятор 3600 мАч, 2 SIM", category, 1000, 5);
         product2 = new Product("Sony Xperia 1 II XQ-AT52", "1_II_XQ-AT52.jpeg",
                 "Android, экран 6.5\" OLED (1644x3840), Qualcomm Snapdragon 865, ОЗУ 12 ГБ, флэш-память 256 ГБ, " +
-                        "карты памяти, камера 12 Мп, аккумулятор 4000 мАч, 2 SIM", category,2679, 19);
+                        "карты памяти, камера 12 Мп, аккумулятор 4000 мАч, 2 SIM", category,2500, 1);
+        product1.setId(1);
+        product2.setId(2);
     }
 
     @BeforeEach
@@ -47,17 +53,17 @@ public class OrderServiceImplTests {
         order = new Order();
         orderService.addProductToBasket(order, product1);
         orderService.addProductToBasket(order, product2);
-        orderService.addProductToBasket(order, product2);
     }
 
     @Test
     public void addProductToBasket_ListSizeTest() {
-        assertEquals(3, order.getProductList().size());
+        assertEquals(2, order.getProductList().size());
     }
 
     @Test
-    public void addProductToBasket_PriceTest() {
-        assertEquals(899 + 2679 + 2679, order.getOrderPrice());
+    public void addProductToBasket_PriceTest() throws ServiceException {
+        orderService.addProductToBasket(order, product2);
+        assertEquals(6000, order.getOrderPrice());
     }
 
     @Test
@@ -69,8 +75,27 @@ public class OrderServiceImplTests {
     @Test
     public void removeProductWithOfBasketByNumber_PriceTest() throws ServiceException, NotFoundException {
         orderService.removeProductWithOfBasketByNumber(order, 1);
-        assertEquals(899 + 2679, order.getOrderPrice());
+        assertEquals(1000, order.getOrderPrice());
+    }
 
+    @Test
+    public void removeProductWithOfBasket_Test() throws ServiceException, NotFoundException {
+        orderService.removeProductWithOfBasket(order, product1);
+        assertEquals(product2, order.getProductList().get(0));
+    }
+
+    @Test
+    public void removeProductWithOfBasket_PriceTest() throws ServiceException, NotFoundException {
+        orderService.removeProductWithOfBasket(order, product1);
+        assertEquals(2500, order.getOrderPrice());
+    }
+
+    @Test
+    public void removeProductWithOfBasket_ExceptionTest() throws ServiceException, NotFoundException {
+        orderService.removeProductWithOfBasket(order, product1);
+        Throwable exception = assertThrows(NotFoundException.class,
+                () -> orderService.removeProductWithOfBasket(order, product1));
+        assertEquals("Will not find a product to remove: " + product1.getName(), exception.getMessage());
     }
 
     @AfterAll
